c0a8deb34c0d14b284e3358162ba240675d1bcdb
Frank Chen <frankchn@gmail.com>
2012-05-19 00:21:32 +0000

    more things

--- class/modules/contest_debug/DebugTeamFrontend.php
+++ class/modules/contest_debug/DebugTeamFrontend.php
@@ -17,7 +17,7 @@ class DebugTeamFrontend extends TeamFrontend {
      </div>
      <div>
        Solution:
-       <select name="solution_type">
+       <select name="solution_type" id="soln_type">
          <option value="correct">Always Correct</option>
          <option value="sometimes">Sometimes Wrong</option>
          <option value="wrong">Always Wrong</option>
@@ -25,14 +25,26 @@ class DebugTeamFrontend extends TeamFrontend {
      </div>
      <div>
         Good Input:
-        <textarea style="width:95%;height:100px" name="good"></textarea>
+        <textarea style="width:95%;height:100px" id="soln_good" name="good"></textarea>
      </div>
      <div>
         Bad Input:
-        <textarea style="width:95%;height:100px" name="bad"></textarea>
+        <textarea style="width:95%;height:100px" id="soln_bad" name="bad"></textarea>
+     </div>
+     <div style="text-align:center">
+        <input type="submit" value="Submit Solution" onClick="submitDebugSolution()">
      </div>
   </div>
 </div>
+<script type="text/javascript">
+
+function submitDebugSolution() {
+  $.ajax({
+    data: $.stringifyJSON({'action': 
+  });
+}
+
+</script>
 <?php
 	}
 
