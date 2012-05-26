f364cbfcd625e7493b8bbe637bbf096a18642a31
Frank Li <strategist333@gmail.com>
2012-05-25 17:09:48 -0700

    Rename zip to packet

--- class/modules/contest_default/problem_packet/PacketProblemTeamDisplay.php
+++ class/modules/contest_default/problem_packet/PacketProblemTeamDisplay.php
@@ -1,13 +1,13 @@
 <?php
 require_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'common.php');
 
-class ZipProblemTeamDisplay extends ProblemTeamDisplay {
+class PacketProblemTeamDisplay extends ProblemTeamDisplay {
   
   public function render() {
     $linkID = parent::transformID('link');
 // BEGIN RENDER
 ?>
-<div class="problem_link"><a id="<?= $linkID ?>" href="<?= $this->url ?>">Download archive</a><div class="zip_line"></div></div>
+<div class="problem_link"><a id="<?= $linkID ?>" href="<?= $this->url ?>"><?= $this->title ?></a><div class="zip_line"></div></div>
 <?php
 // END RENDER
   }
